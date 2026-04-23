5c50a33564f45411bca7a2df5afdb456f5bbc9e9
Marcus Chan <chanmarcus53@gmail.com>
2026-04-22 23:53:40 -0700

    finished updating descriptive comments

--- Assignment2.py
+++ Assignment2.py
@@ -7,9 +7,9 @@ from sklearn.feature_selection import chi2, SelectKBest
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, StackingClassifier, AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import BaggingClassifier, ExtraTreesClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import RidgeClassifier, 
+from sklearn.linear_model import RidgeClassifier
 from sklearn.svm import SVC
 
 import matplotlib.pyplot as plt
@@ -25,8 +25,23 @@ pd.set_option('display.width', 1000)
 
 #Function used to compile results and complete test evaluation
 def find_results(model, X_data, y_data, output_type='binary', x_scaler=None):
-    #TODO: integrate code to handle scalers
-    # applying scaler (optional)
+    """
+    This fuction will take in a model, the x and y data, the type of output (binary or micro) 
+    and an optional scaler for the x data. It will then perform KFold validation on the data 
+    and return the average and standard deviation of accuracy, recall, precision, f1 score and rmse.
+
+    Parameters:
+    - model: the machine learning model to be tested
+    - X_data: the input features for the model
+    - y_data: the target variable for the model
+    - output_type: the type of output for the scoring functions (binary, micro, macro
+    - x_scaler: an optional scaler for the x data (MinMaxScaler, StandardScaler, RobustScaler)
+
+    Reponses:
+    - A list containing the average and standard deviation of accuracy, recall, precision, f1 score and rmse in the following order:
+        [average_accuracy, stdev_accuracy, average_recall, stdev_recall, average_precision, stdev_precision,
+        average_f1, stdev_f1, average_rmse, stdev_rmse]
+    """
 
     # this function will take the model, x and y data then test them though Kfold validation
     kf = KFold(n_splits=6, shuffle=True, random_state=123)
@@ -71,8 +86,11 @@ def find_results(model, X_data, y_data, output_type='binary', x_scaler=None):
     stdev_f1 = np.std(store_scores[3][:])
     stdev_rmse = np.std(store_scores[4][:])
 
+    ## Optional print the confusion matrix for the last fold of KFold validation, 
+    ## this is just to get a better understanding of the model performance
     # print_confusion(y_test, predictions)
 
+    ## Optional print the results in a nice format, this is just for testing purposes and can be removed for the final version
     # print(f"Accuracy:   {average_accuracy} +/- {stdev_accuracy}")
     # print(f"Recall:     {average_recall} +/- {stdev_recall}")
     # print(f"Precision:  {average_precision} +/- {stdev_precision}")
@@ -83,13 +101,33 @@ def find_results(model, X_data, y_data, output_type='binary', x_scaler=None):
             average_f1, stdev_f1, average_rmse, stdev_rmse]
 
 def print_confusion(y_test, y_pred):
+    """
+    Prints the confusion matrix for the given true labels and predicted labels.
+
+    Parameters:
+    - y_test: the true labels
+    - y_pred: the predicted labels
+
+    Reponses:
+    - None, but prints the confusion matrix to the console
+    """
     cm = confusion_matrix(y_test, y_pred)
     print(cm)
 
-def showVIF(X): # need to rewrite this in my own code # this is a placeholder
-    # this doesn't work becuase VIF score doesn't handle boolean well, convert true/false column to float
-    # acedemic work impact no/ yes
-    # genders as well
+def showVIF(X): 
+    """
+    showVIF function takes in a dataframe of features and calculates the Variance Inflation Factor (VIF) for each feature.
+    VIF is a measure of multicollinearity in a dataset, which can indicate if there are any features that are highly 
+    correlated with each other.
+    A VIF value greater than 5 or 10 is often considered to indicate high multicollinearity, which can lead to issues 
+    in regression models.
+
+    Parameters:
+    - X: a dataframe of features for which to calculate VIF
+
+    Reponses:
+    - None, but prints a dataframe of features and their corresponding VIF values to the console
+    """
     vif_df = pd.DataFrame()
     vif_df["Feature"] = X.columns
     vif_df["VIF"] = [variance_inflation_factor(X.values, i) for i in range(len(X.columns))]
@@ -98,12 +136,34 @@ def showVIF(X): # need to rewrite this in my own code # this is a placeholder
     print(vif_df)
 
 def display_complied_reports(report_list):
+    """
+    Displays the compiled reports in a formatted table.
+
+    Parameters:
+    - report_list: a list of lists, where each inner list contains the model name and its
+    corresponding metrics (accuracy, recall, precision, f1 score, rmse) along with their standard deviations.
+
+    Reponses:
+    - None, but prints a formatted table of the model names and their corresponding metrics to the console.
+    """
     #columns = ['Acc', 'Acc Stdev', 'Recall', 'Recall Stdev', 'Prec', 'Prec Stdev', 'F1', 'F1 Stdev', 'RMSE', 'RMSE Stdev']
     print("MODEL \t ACC\tSTDEV\tREC\tSTDEV\tPREC\tSTDEV\tF1\tSTDEV\tRMSE\tSTDEV")
     for report in report_list:
         print(f"{report[0]:.4}\t{report[1]:.4}\t{report[2]:.4}\t{report[3]:.4}\t{report[4]:.4}\t{report[5]:.4}\t{report[6]:.4}\t{report[7]:.4}\t{report[8]:.4}\t{report[9]:.4}\t{report[10]:.4}")
         
 def feature_selection(X, y, n=10):
+    """
+    This function performs feature selection using the chi-squared test. It selects the top n features that are most
+    correlated with the target variable y.
+
+    Parameters:
+    - X: a dataframe of features
+    - y: the target variable
+    - n: the number of top features to select (default is 10)
+
+    Reponses:
+    - A dataframe containing only the selected features.
+    """
     selector = SelectKBest(chi2, k=n)
     select_X = selector.fit_transform(X, y)
     selected_features = X.columns[selector.get_support()]
@@ -113,6 +173,17 @@ def feature_selection(X, y, n=10):
     return X
 
 def createStackingClassifier():
+    """
+    This function creates a Stacking Classifier using a list of base models and a meta model. 
+    The base models are trained on the original dataset, and their predictions are used as 
+    input for the meta model, which makes the final prediction.
+
+    Parameters:
+    - None
+
+    Reponses:
+    - A StackingClassifier object that can be used for training and prediction.
+    """
     # Define base models
     # base_models = [
     #     ('dt', DecisionTreeClassifier()),
@@ -146,12 +217,12 @@ def createStackingClassifier():
 
     return stack_classifier
     
-
-
-
+# Data Preprocessing
 print(df['addiction_level'].unique())
 print(df['stress_level'].unique())
 
+# we have to convert the addiction level and stress level to numerical values for the models to be able 
+# to process them, we will use a simple mapping for this
 addiction_level_map = {'None': 0, 'Mild': 1, 'Moderate': 2,'Severe': 3}
 df['addiction_level'] = df['addiction_level'].map(addiction_level_map)
 
@@ -227,6 +298,8 @@ en          = LogisticRegression(penalty='elasticnet', solver='saga')
 ada         = AdaBoostClassifier()
 stack       = createStackingClassifier()
 
+# compiling the models into a dictionary for easier testing and reporting, this will allow us to 
+# easily loop through the models and test them with both the binary and micro output types
 model_dict = {"Decision Tree": dt, 
               "Logistic Regression": lr, 
               "Random Forest": rf, 
@@ -238,6 +311,8 @@ model_dict = {"Decision Tree": dt,
               "AdaBoost": ada,
               "Stacking": stack}
 
+# Testing the models with both the binary and micro output types, this will give us a better understanding 
+# of the model performance on both the addicted_label and addiction_level targets
 for key, value in model_dict.items():
     model_name = key
     print(f"++++++++++++++++{model_name}====================")
